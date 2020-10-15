a2321627deca08ce22c42eaad84490d5d8d6f448
Pooria Farhad <pooriaf@gmail.com>
2020-10-15 17:23:25 +0330

    bypass non text/html contents

--- src/CrawlerService/AbstractWebLookup.php
+++ src/CrawlerService/AbstractWebLookup.php
@@ -4,9 +4,15 @@ namespace App\CrawlerService;
 
 abstract class AbstractWebLookup
 {
-    public function fetchContent(string $url): ?string
+    public function fetchContent(string $url): string
     {
-        return file_get_contents($url);
+        $headers = get_headers($url, 1);
+        $contentType = is_array($headers['Content-Type']) ? $headers['Content-Type'][0] : $headers['Content-Type'];
+        if (strpos($contentType, 'text/html') === false) {
+            return '';
+        }
+
+        return @file_get_contents($url);
     }
 
     public function emailLookup(string $content): array
