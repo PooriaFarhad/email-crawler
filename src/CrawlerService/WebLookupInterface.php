5ed730190ada5bb4676134013b1236099be8154b
Pooria Farhad <pooriaf@gmail.com>
2020-10-15 10:52:25 +0330

    add web lookup interface

---
+++ src/CrawlerService/WebLookupInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\CrawlerService;
+
+
+interface WebLookupInterface
+{
+    public function fetchContent(string $url): ?string;
+
+    public function emailLookup(string $content): array;
+
+    public function urlLookup(string $domain, string $content): array;
+}
